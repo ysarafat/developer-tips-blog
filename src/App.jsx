d365698f8ed1869188d0bacd6cca00f2dfdb02bf
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-03-31 15:16:09 +0600

    localStroage Integrate and data display on ui

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState([]);
-  const [read ,setRead] =useState(0)
+  const [read ,setRead] =useState([])
   const [cart, setCart] =useState([])
   useEffect(()=>{
     fetch('data.json')
@@ -17,19 +17,28 @@ function App() {
   },[]);
 
 const readTime = (blog) => {
-  toast("Reading time added successfully");
+  
   const previousReadingTime = JSON.parse(localStorage.getItem("reading-time"));
+  let readingTime =[];
   if (previousReadingTime){
-    const sum = previousReadingTime + blog.reading;
-    localStorage.setItem("reading-time",sum)
-    setRead(sum)
-  } else{
-    localStorage.setItem("reading-time",blog.reading)
-    setRead(blog.reading)
+    const isThisMark = previousReadingTime.find(bl => bl.id === blog.id);
+    if(isThisMark){
+      toast("Already Bookmarked Spent Time.")
+    } else{
+      readingTime.push(...previousReadingTime,blog)
+      localStorage.setItem("reading-time" , JSON.stringify(readingTime))
+      toast("Reading time added successfully");
+    }
+  } else {
+    readingTime.push(blog)
+    localStorage.setItem("reading-time" , JSON.stringify(readingTime))
+    toast("Reading time added successfully");
   }
+  setRead(readingTime)
 }
 
 const handelCart = (blog) => {
+  
   const previousBookmark = JSON.parse(localStorage.getItem("bookmark"));
   let bookmark = [];
   if (previousBookmark){
@@ -39,14 +48,16 @@ const handelCart = (blog) => {
     } else{
       bookmark.push(...previousBookmark,blog)
       localStorage.setItem("bookmark" , JSON.stringify(bookmark))
+      toast("Bookmark added successfully");
     }
-  } else {
+  } else { 
     bookmark.push(blog)
     localStorage.setItem("bookmark" , JSON.stringify(bookmark))
+    toast("Bookmark added successfully");
   }
   setCart(previousBookmark)
+  
 }
-
   return (
     <>
      <Navbar />
--- src/Components/side-cart/Sidecart.jsx
+++ src/Components/side-cart/Sidecart.jsx
@@ -4,19 +4,25 @@ const Sidecart = ({read, cart}) => {
     const [time, setTime] = useState(read)
     const [cartinfo, setCartinfo] = useState(cart)
 
-    useEffect(()=>{
-        const getReadTime = localStorage.getItem("reading-time")
-        setTime(getReadTime);
-    },[read])
     useEffect(()=> {
         const getItemFromLocal = JSON.parse(localStorage.getItem("bookmark"))
         setCartinfo(getItemFromLocal)
     },[cart])
-console.log(cartinfo);
+    useEffect(()=> {
+        const getReading = JSON.parse(localStorage.getItem("reading-time"))
+        setTime(getReading)
+    },[read])
+    let totalRead = 0;
+    if (time){
+        for (const readingTime of time){
+            totalRead = totalRead + readingTime.reading;
+        }
+    }
     return (
         <div className=' md:sticky top-[66px] px-5'>
             <div className='border border-[#6047EC] bg-[#6047EC1A] px-12 py-5 rounded-lg mb-7'>
-                <h2 className='text-2xl text-[#6047EC] font-bold'>Spent time on read : {time? time : 0} </h2>
+                
+                <h2 className='text-2xl text-[#6047EC] font-bold'>Spent time on read : {totalRead? totalRead : 0} </h2>
             </div>
             <div className='bg-[#1111110d] p-7 rounded-lg  md:mb-0 mb-5'>
                 <h1 className='text-2xl font-bold mb-4'>Bookmarked Blogs : {cartinfo?.length ? cartinfo.length : 0 }</h1>
